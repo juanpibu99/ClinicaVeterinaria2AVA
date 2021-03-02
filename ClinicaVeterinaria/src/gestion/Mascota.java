1fd921def6bb999d3bf81e0b3a8956e0492e6589
jpinyerob <jpinyerob@lmde>
2021-03-02 20:40:21 +0100

    sg

--- ClinicaVeterinaria/src/gestion/Mascota.java
+++ ClinicaVeterinaria/src/gestion/Mascota.java
@@ -1,12 +1,26 @@
 package gestion;
 
 import java.io.Serializable;
+/**
+ * Aqui tenemos una clase mascota la cual es un objeto que realiza la funcion de una mascota que 
+ * tiene un dueño.
+ * 
+ * @author jpinyerob
+ *@version 1.0 2021/02/02
+ */
 
 public class Mascota extends Animal {
-
+	//Aqui estan los atributos de la clase
 	private String nombre;
     private Persona duenyo;
-
+    /**
+     * Aqui esta el metodo constructor
+     * @param ID
+     * @param nombre
+     * @param edad
+     * @param raza
+     * @param duenyo
+     */
     public Mascota(long ID, String nombre, int edad, String raza, Persona duenyo) {
     	this.setID(ID);
         this.nombre = nombre;
@@ -14,23 +28,37 @@ public class Mascota extends Animal {
         this.raza = raza;
         this.duenyo = duenyo;
     }
-
+    /**
+     * Aqui esta el getter del atributo nombre
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Aqui esta el setter del atributo nombre
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * Aqui esta el dueño de una mascota
+     * @return
+     */
     public Persona getDuenyo() {
         return duenyo;
     }
-
+    /**
+     * Aqui esta el setter del dueño de una mascota
+     * @param Persona
+     */
     public void setDuenyo(Persona duenyo) {
         this.duenyo = duenyo;
     }
-
+/**
+ * Aqui esta el toString de la clase
+ */
     @Override
     public String toString() {
         return ("Nombre: " + nombre + " Edad: " + edad + " Raza: " + raza + " Due�o: " + duenyo.toString());
--- ClinicaVeterinaria/src/gestion/Persona.java
+++ ClinicaVeterinaria/src/gestion/Persona.java
@@ -1,57 +1,98 @@
 package gestion;
 
 import java.io.Serializable;
-
+/**
+ * Aqui esta la clase persona donde se guardan todos los datos de la misma
+ * @author jpinyerob
+ *@version 1.0 2021/02/02
+ */
 public class Persona implements Serializable{
+	//Aqui estan los atributos de la clase
 	private static final long serialVersionUID = 1L;
     private String nombre;
     private String apellidos;
     private String dni;
     private int edad;
-
+    /**
+     * Aqui esta el constructor de la clase
+     * @param nombre
+     * @param apellidos
+     * @param dni
+     * @param edad
+     */
     public Persona(String nombre, String apellidos, String dni, int edad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.dni = dni;
         this.edad = edad;
     }
-
+/**
+ * getter de nombre
+ * @return
+ */
     public String getNombre() {
         return nombre;
     }
-
+/**
+ * setter de nombre
+ * @param nombre
+ */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    /**
+     * getter de Apellidos
+     * @return
+     */
     public String getApellidos() {
         return apellidos;
     }
-
+    /**
+     * setter de Apellidos
+     * @param nombre
+     */
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
     }
-
+    /**
+     * getter de DNI
+     * @return
+     */
     public String getDni() {
         return dni;
     }
-
+    /**
+     * setter de DNI
+     * @param nombre
+     */
     public void setDni(String dni) {
         this.dni = dni;
     }
-
+    /**
+     * getter de Edad
+     * @return
+     */
     public int getEdad() {
         return edad;
     }
-
+    /**
+     * setter de edad
+     * @param nombre
+     */
     public void setEdad(int edad) {
         this.edad = edad;
     }
-
+/**
+ * te devuelve el año de nacimiento de alguien
+ * @param anyoactual
+ * @return
+ */
     public int dameAnyoNacimiento(int anyoactual) {
         return (anyoactual - this.getEdad());
     }
-    
+    /**
+     * metodo toString de la clase
+     */
     @Override
     public String toString() {
         return("Nombre: " + nombre + " Apellidos: " + apellidos + " DNI: " + dni + " Edad: " + edad);        
