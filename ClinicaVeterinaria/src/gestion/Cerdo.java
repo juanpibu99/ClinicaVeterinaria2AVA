c4a9f89aea7a8597251c6d93b962a10289f993a6
jpinyerob <jpinyerob@lmde>
2021-03-02 19:44:26 +0100

    examen

---
+++ ClinicaVeterinaria/src/gestion/Cerdo.java
@@ -0,0 +1,36 @@
+package gestion;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author xavier
+ */
+public class Cerdo extends Mascota{
+    //ATRIBUTOS
+    private float peso;
+    
+    //CONSTRUCTOR
+    /**
+     * Constructor de la clase Cerdo que hereda de Mascota
+     * @param ID
+     * @param nombre
+     * @param edad
+     * @param raza
+     * @param duenyo
+     * @param peso 
+     */
+    public Cerdo(long ID, String nombre, int edad, String raza, Persona duenyo, float peso) {
+        super(ID, nombre, edad, raza, duenyo);
+        this.setPeso(peso);
+    }
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+}
